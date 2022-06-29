1aefda6aacbe03be2ea99472d16405d572c846a5
zieunx <wldms_1217@naver.com>
2022-06-29 22:33:31 +0900

    add: init code project

---
+++ head-first-design-patterns/src/test/java/com/book/headfirstdesignpatterns/HeadFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.headfirstdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
